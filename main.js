a2ab037fb57945ccec0bc133c9f553aec38a75ec
Luka <lukagaxokidze28@gmail.com>
2024-09-18 23:06:54 +0400

    lesson1

---
+++ main.js
@@ -0,0 +1,36 @@
+let userEmail = "lukagaxokidze28@gmail.com";
+let userPassowrd = "12345678";
+
+if (userPassowrd === "12345678" && userEmail === "lukagaxokidze28@gmail.com") {
+  console.log("Signed In !");
+} else if (!userPassowrd === "12345678") {
+  console.log("passowrd is invalid");
+} else if (!userEmail === "lukagaxokidze28@gmail.com") {
+  console.log("email is invalid");
+} else {
+  console.log("error check email/passowrd");
+}
+
+//
+
+let balance = 300;
+
+let item1 = 150;
+let item2 = 100;
+let item3 = 25;
+
+if (balance > item1 && item2 && item3) {
+  let change = balance - item1 && item2 && item3;
+  console.log("Your change is: " + change);
+}
+
+//
+
+let checkRefreshToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+if (checkRefreshToken) {
+  console.log("user is signed in");
+} else {
+  console.log("user does not exists");
+}
